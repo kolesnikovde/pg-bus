f1d22c96b91e14ae07dc884d6ffd36e5d0761ccf
Kolesnikov Danil <kolesnikovde@gmail.com>
2015-09-19 21:34:17 +0300

    Added tests.

---
+++ test.js
@@ -0,0 +1,28 @@
+var pg     = require('pg');
+var assert = require('assert');
+var bus    = require('./');
+
+describe('pg-bus', function() {
+  var url = process.env.DB || 'postgres://localhost/postgres',
+      db;
+
+  beforeEach(function(done) {
+    pg.connect(url, function(err, conn) {
+      if (err) throw err;
+
+      db = conn;
+      done();
+    });
+  });
+
+  it('sends and receives notifications from channel', function(done) {
+    var chan = bus(db, '0');
+
+    chan.listen('data', function(data) {
+      assert.deepEqual(data, { foo: "'; die; --" });
+      done();
+    });
+
+    chan.notify('data', { foo: "'; die; --" });
+  });
+});
